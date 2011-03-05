289d714896500247467c5cbc6c6aa5b562438cc8
Pedro Melo <melo@simplicidade.org>
2011-03-05 22:12:37 +0000

    Build results of 31265d2 (on master)

--- lib/Dist/Zilla/Plugin/BugTracker/FogBugz.pm
+++ lib/Dist/Zilla/Plugin/BugTracker/FogBugz.pm
@@ -1,4 +1,7 @@
 package Dist::Zilla::Plugin::BugTracker::FogBugz;
+BEGIN {
+  $Dist::Zilla::Plugin::BugTracker::FogBugz::VERSION = '0.001';
+}
 
 # ABSTRACT: Use FogBugz as the bug tracker for your distribution
 
@@ -7,3 +10,29 @@ use warnings;
 
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Plugin::BugTracker::FogBugz - Use FogBugz as the bug tracker for your distribution
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Pedro Melo <melo@simplicidade.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2011 by Pedro Melo.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0
+
+=cut
+
